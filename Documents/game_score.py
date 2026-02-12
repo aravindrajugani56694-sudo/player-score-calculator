8400770e2650913f2ee2af000803d920d8cabb4e
Aravind Rajugani <aravindrajugani56694@gmail.com>
2026-02-12 13:20:37 +0530

    Initial commit

---
+++ Documents/game_score.py
@@ -0,0 +1,12 @@
+player_name = input("Enter player name: ")
+games_played = int(input("Enter number of games played: "))
+total_score = int(input("Enter the total score achieved: "))
+average_score = total_score / games_played
+
+
+
+print("\n--- Player Statisics---")
+print(f"Player Name: {player_name}")
+print(f"Games Played: {games_played}")
+print(f"Total Score: {total_score}")
+print(f"Average Score: {average_score}")
